43a6ca038bba162d2eb12118f600381a9f1a823a
tkkwon1998 <tkkwon1998@gmail.com>
2020-04-04 12:31:24 -0400

    Updated to fit new API

--- src/Maps.js
+++ src/Maps.js
@@ -38,7 +38,7 @@ class Maps extends Component {
             return Object.values(item);
           });
 
-        var idxToDelete = [0,3,4,5,6,8,9,10,11,12,13,14,15,16];  // array of columns to delete
+        var idxToDelete = [0,3,4,5,6,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24];  // array of columns to delete
 
         for (var i = 0; i < array.length; i++) {    // loop to delete indices from array
             var temp = array[i];
